46ea1a6b86ab5ca3f8f35f1fa1972f21411a24bb
Brooke Wright <brookerwright2@gmail.com>
2023-10-14 16:45:28 -0400

    Rotate the ball frog baby stands on when she moves

--- project4/frog_baby/frog_baby.gd
+++ project4/frog_baby/frog_baby.gd
@@ -4,11 +4,14 @@ extends CharacterBody2D
 
 const _SPEED := 300.0
 const _JUMP_VELOCITY := -400.0
+const _ROTATION_PER_SECOND := PI
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var _right := true
+
+@onready var ball : Sprite2D = $BallSprite
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -24,7 +27,9 @@ func _physics_process(delta):
 
 	if _right:
 		velocity.x = _SPEED
+		ball.rotate(_ROTATION_PER_SECOND * delta)
 	else:
 		velocity.x = -_SPEED
-
+		ball.rotate(-_ROTATION_PER_SECOND * delta)
+	
 	move_and_slide()
