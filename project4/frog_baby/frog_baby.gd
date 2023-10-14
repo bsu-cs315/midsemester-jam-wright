e368dccdd82b18c0f951f056153406654466ee90
Brooke Wright <brookerwright2@gmail.com>
2023-10-14 15:05:29 -0400

    Create a Character that moves back and forth

---
+++ project4/frog_baby/frog_baby.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+const _SPEED := 300.0
+const _JUMP_VELOCITY := -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var _gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var _right := true
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += _gravity * delta
+
+	# Handle Jump.
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		velocity.y = _JUMP_VELOCITY
+	# Bounce back and forth direction
+	if is_on_wall():
+		_right = !_right
+
+	if _right:
+		velocity.x = _SPEED
+	else:
+		velocity.x = -_SPEED
+
+	move_and_slide()
