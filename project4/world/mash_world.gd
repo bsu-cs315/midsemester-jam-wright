ab3b1e185cc80e8cdac1f6e018f4cfa8e0932637
Brooke Wright <brookerwright2@gmail.com>
2023-10-14 20:15:41 -0400

    Move percentage_rubbed to a singleton script

--- project4/world/mash_world.gd
+++ project4/world/mash_world.gd
@@ -4,7 +4,7 @@ const _MASH_VALUE := 2
 const _ARM_MOVEMENT := 20
 
 
-var percent_rubbed := 0
+
 
 var _arm_direction := 1
 var _mash_allowed := true
@@ -20,17 +20,17 @@ func _ready():
 func _process(_delta):
 	
 	if _mash_allowed and Input.is_action_just_pressed("mash"):
-		percent_rubbed += _MASH_VALUE
+		ScoreKeeper.percent_rubbed += _MASH_VALUE
 		$FrogArm.global_position.x += _ARM_MOVEMENT * _arm_direction
 		_arm_direction *= -1
 		_set_rub_label()
-		if percent_rubbed >= 100:
+		if ScoreKeeper.percent_rubbed >= 100:
 			_mash_allowed = false
 			_rub_label.text = "Nose gone!"
 
 
 func _set_rub_label():
-	_rub_label.text = "Percent Rubbed: %d%%" % percent_rubbed
+	_rub_label.text = "Percent Rubbed: %d%%" % ScoreKeeper.percent_rubbed
 
 
 func _on_leave_scene_timer_timeout():
---
+++ project4/world/score_keeper.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var percent_rubbed := 0
+
+
+func set_percentage_rubbed(new_percent : int) -> void:
+	percent_rubbed = new_percent
+
+
+func get_percentage_rubbed() -> int:
+	return percent_rubbed
