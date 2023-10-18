c529d3a0d8a54843f6092dcade6ca0f20f1a93b6
Brooke Wright <brookerwright2@gmail.com>
2023-10-18 10:14:24 -0400

    Randomly spawn Frog Baby at different points

--- project4/world/world.gd
+++ project4/world/world.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 
-@onready var music : AudioStreamPlayer = $Music
-
+@onready var _music : AudioStreamPlayer = $Music
+@onready var _spawn_points := $SpawnPoints.get_children()
 
 func _ready() -> void:
 	# Fade in the music
-	music.set_volume_db(-10)
+	_music.set_volume_db(-10)
 	var tween := create_tween()
-	tween.tween_property(music, "volume_db", 0, .5)
+	tween.tween_property(_music, "volume_db", 0, .5)
+	$FrogBaby.global_position = _spawn_points.pick_random().global_position
 
 
 func _on_frog_mash_requested() -> void:
